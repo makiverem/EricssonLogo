ad122c5b45049ddcfbbd5d2e975c88dd183241cf
galovics <galovicsarnold@gmail.com>
2013-11-20 11:09:10 +0100

    Decreased the size of source code.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <cstdio>
 
-void a(char m[97][72], int s, int e, int r) {
-	for(int i = s;i<e;++i)
-		m[r][i] = '#';
+typedef int s;
+
+
+void a(s m[97][72], s k, s e, s r) {
+	for(s i = k;i<e;++i)
+		m[r][i] = 35;
 }
-void d(char m[97][72], int r) {
-	int b[39][2] = {
+void d(s m[97][72], s r) {
+	s b[39][2] = {
 			{56, 64}, {53, 66}, {51, 67}, {48, 68}, {45, 69},
 			{42, 70}, {39, 70}, {37, 71}, {34, 71}, {31, 71},
 			{28, 71}, {26, 71}, {23, 71}, {20, 71}, {17, 70},
@@ -15,20 +18,20 @@ void d(char m[97][72], int r) {
 			{0, 37}, {0, 34}, {1, 32}, {1, 29}, {2, 26},
 			{3, 23}, {4, 20}, {5, 18}, {7, 15}
 	};
-	for(int i=0;i<39;++i)
+	for(s i=0;i<39;++i)
 		a(m, b[i][0], b[i][1], r++);
 }
-int main() {
-	char m[97][72];
-	for (int i=0;i<97;++i)
-		for (int j=0;j<72;++j)
-			m[i][j]=' ';
+s main() {
+	s m[97][72];
+	for (s i=0;i<97;++i)
+		for (s j=0;j<72;++j)
+			m[i][j]=32;
 	d(m,0);
 	d(m,29);
 	d(m,58);
 
-	for (int i=0;i<97;++i) {
-		for (int j=0;j<72;++j)
+	for (s i=0;i<97;++i) {
+		for (s j=0;j<72;++j)
 			printf("%c", m[i][j]);
 		printf("\n");
 	}
